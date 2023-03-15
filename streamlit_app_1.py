e7b4b6750433cfab00427f97778599c7c96db7ca
Sushmitha Puneeth <122335181+Sushmithapuneeth@users.noreply.github.com>
2023-03-15 11:46:31 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -6,3 +6,5 @@ streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
 streamlit.text(" 🥗 Kale, Spinach & Rocket smothie")
 streamlit.text(" 🐔 Hard-boiled free Range EGG!")
 streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
